e0864f739aa7c496b8586777586dbc9b9e892621
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2024-10-03 17:17:55 +0200

    Added more properties to the dictionary.
    Added CHOICE2 "2. Settings" and CHOICE3 "3. Exit game" to the "en" property inside the dictionary. Then added CHOICE2 "2. Innstillinger" and CHOICE3 "3. Avslutt spill" to the "no" property inside the dictionary.

--- language.mjs
+++ language.mjs
@@ -4,6 +4,8 @@ const DICTIONARY = {
         PLAY_AGAIN_QUESTION: "Play again (YES/no)? ",
         CONFIRM: "y",
         CHOICE1: "1. Play Game",
+        CHOICE2: "2. Settings",
+        CHOICE3: "3. Exit game",
         PLASS: "Place your mark at: "
     },
     },
@@ -11,6 +13,8 @@ const DICTIONARY = {
         PLAY_AGAIN_QUESTION: "Spille en gang til (Ja/nei)? ",
         CONFIRM: "j",
         CHOICE1: "1. Spill",
+        CHOICE2: "2. Innstillinger",
+        CHOICE3: "3. Avslutt spill",
         PLASS: "Velg plassering:"
     }
 }
