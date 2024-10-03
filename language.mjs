8a98d751fc2a1a9f43fe40aa5866c76823f2d29a
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2024-10-03 15:52:28 +0200

    Added properties to the dictionary.
    Added properties for "Play game" called CHOICE1 and for "place your marker" called PLASS inside the dictionary in both the english and norwegian properties called "en" and "no"

--- language.mjs
+++ language.mjs
@@ -3,10 +3,15 @@ const DICTIONARY = {
     en: {
         PLAY_AGAIN_QUESTION: "Play again (YES/no)? ",
         CONFIRM: "y",
+        CHOICE1: "1. Play Game",
+        PLASS: "Place your mark at: "
+    },
     },
     no: {
         PLAY_AGAIN_QUESTION: "Spille en gang til (Ja/nei)? ",
         CONFIRM: "j",
+        CHOICE1: "1. Spill",
+        PLASS: "Velg plassering:"
     }
 }
 
